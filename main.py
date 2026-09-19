ebf8ae2d9ef07437c781586c6edb5f3dce636008
Ilia Amirejibi <iliaamirejibi@Ilias-MacBook-Air.local>
2026-09-19 18:16:15 +0200

    switched to railway

--- main.py
+++ main.py
@@ -39,4 +39,5 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000) 
+    port = int(os.environ.get('PORT', 8000))
+    app.run(debug=not IS_PRODUCTION, host='0.0.0.0', port=port) 
